2b0aec6a2b1e8c72790d58870000b07a84cc8c6c
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-03-30 07:52:48 -0300

    criacao do programa que calcula sua idade a partir do ano de nascimento

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write($"Digite o seu nome: ");
+string? nome = Console.ReadLine();
+
+Console.Write("Digite o ano de seu nascimento: ");
+int datau = int.Parse(Console.ReadLine());
+
+var data = DateTime.Now;
+
+var datas = (data.Year);
+
+int idade = datas - datau;
+
+int semana = idade * 52;
+
+Console.WriteLine($"{nome}, voce tem {idade} anos de idade, e durante esse tempo, voce viveu durante {semana} semanas.");
+
+
+
+
